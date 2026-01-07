f9fa43f08d6244fcd46afc2e78156c1522584cac
Pedro-DiasZ <pedrohenriquediasmoreira1@gmail.com>
2026-01-07 13:52:45 -0300

    Update models.py

--- models.py
+++ models.py
@@ -6,3 +6,5 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), nullable=False)
     description = db.Column(db.String(300))
+    completed = db.Column(db.Boolean, default=False)
+    user_owner = db.Column(db.String(100), nullable=True)
