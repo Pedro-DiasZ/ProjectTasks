b684412c5d62cf2c79d4cde1fc9ede015f49c50a
Pedro-DiasZ <pedrohenriquediasmoreira1@gmail.com>
2026-01-07 13:55:36 -0300

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -13,6 +13,16 @@ function togglemode() {
     }
 }
 
+window.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem("theme");
+    
+    if (savedTheme === "dark") {
+        document.documentElement.classList.add("dark");
+    } else {
+        document.documentElement.classList.remove("dark");
+    }
+});
+
 
 function logoutUser() {
     window.location.href = 'index.html'; 
