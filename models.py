162b8ffd7dc5faa5179262816a704f1c965aca84
Pedro-DiasZ <pedrohenriquediasmoreira1@gmail.com>
2026-01-12 09:08:15 -0300

    Add User model with password verification

--- models.py
+++ models.py
@@ -1,10 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    senha = db.Column(db.String(255), nullable=False)
+
+    tasks = db.relationship('Task', backref='user', lazy=True)
+    
+    def verify_password(self, password):
+        return check_password_hash(self.senha, password)
+
+
 class Task(db.Model):
+    __tablename__ = 'tasks'
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), nullable=False)
-    description = db.Column(db.String(300))
     completed = db.Column(db.Boolean, default=False)
-    user_owner = db.Column(db.String(100), nullable=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
