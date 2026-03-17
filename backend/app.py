b07a0a15d62919eb79bbcb784051fabadaa08091
Pedro-DiasZ <pedrohenriquediasmoreira1@gmail.com>
2026-03-17 15:20:48 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from models import db, Task, User
+from .models import db, Task, User
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from datetime import timedelta
